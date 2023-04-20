b96c4717260dcd032ad79afad603acdc212f4c39
cema <cema@itu.dk>
2023-04-20 17:05:50 +0200

    Add some hardcoded markers with colours and titles

--- app/src/main/java/com/example/stickerapp/MapFragment.java
+++ app/src/main/java/com/example/stickerapp/MapFragment.java
@@ -13,6 +13,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -22,20 +23,20 @@ public class MapFragment extends Fragment {
 
         String apiKey = BuildConfig.API_KEY;
 
+        LatLng startPos = new LatLng(55.658619, 12.589548);
+
         /**
          * Manipulates the map once available.
          * This callback is triggered when the map is ready to be used.
          * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
          * If Google Play services is not installed on the device, the user will be prompted to
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            setAllPins(googleMap);
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(startPos, 15));
         }
     };
 
@@ -55,5 +56,20 @@ public class MapFragment extends Fragment {
         if (mapFragment != null) {
             mapFragment.getMapAsync(callback);
         }
+    }
+
+    public void setAllPins(GoogleMap googleMap) {
+        LatLng icecreamSticker = new LatLng(55.660505, 12.591268);
+        googleMap.addMarker(new MarkerOptions().position(icecreamSticker).title("A removed sticker").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE)));
+
+        LatLng metroSticker = new LatLng(55.655954,12.589270);
+        googleMap.addMarker(new MarkerOptions().position(metroSticker).title("A removed sticker").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE)));
+
+        LatLng jurSticker = new LatLng(55.661571,12.586713);
+        googleMap.addMarker(new MarkerOptions().position(jurSticker).title("A sticker").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+    }
+
+    public void placePin(GoogleMap googleMap, double lat, double lng, String title, String colour) {
+
     }
 }
