0d4e7b2824949506b3139f2970dcf4b5c3bdd248
cema <cema@itu.dk>
2023-05-02 13:53:32 +0200

    Add fragments for debrief message and notifying that counter went up

---
+++ app/src/main/java/com/example/stickerapp/CountUpFragment.java
@@ -0,0 +1,25 @@
+package com.example.stickerapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CountUpFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_count_up, container, false);
+        //
+        return v;
+    }
+}
