32a46b56736ea3ace7f148b894ea33d40da54f51
cema <cema@itu.dk>
2023-05-19 10:58:29 +0200

    Add comment

--- app/src/main/java/com/example/stickerapp/MainActivity.java
+++ app/src/main/java/com/example/stickerapp/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.stickerapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+/*This is the version I will use for
+* making a screen recording for the exam hand-in.
+* It has hard-coded stickers around the ITU area.
+* */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
